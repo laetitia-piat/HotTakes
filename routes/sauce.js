42abd6eb279d8e9e6281da89ee2f6c70dcbb7dd1
laetitia-piat <63997891+laetitia-piat@users.noreply.github.com>
2023-02-20 21:39:12 +0100

    Add files via upload
    First commit

---
+++ routes/sauce.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+
+const thing = require('../models/Thing');
+const user = require('../models/User');
+
+/*srouter.post('/auth/signup', (req, res, next) => {
+    const User = new User({
+      ...req.body
+    });
+    user.save()
+      .then(() => res.status(201).json({ message: 'user enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+  });*/
+
+  router.post('/sauces',(req,res,next) => {
+    console.log(req.body);
+    res.statut(201).json({
+    message : 'sauce créé!'
+    });
+  });
+
+  /*router.get('/sauces', (req, res, next) => {
+    const Sauce = new Sauce({
+      ...req.body
+    });
+    sauce.save()
+      .then(sauce => res.status(200).json(sauce))
+      .catch(error => res.status(400).json({ error }));
+  });*/
+
+  /*router.post('/api/auth/signup',(req,res,next)) => {
+  console.log(req.body);
+  res.statut(201).json({
+    message : 'user créé!'
+  })
+};
+
+router.post('/api/auth/login',(req,res,next)) => {
+  
+};
+
+router.get('/api/sauces' ,(req,res,next)) => {
+  
+};
+
+router.get('/api/sauces/:id' ,(req,res,next)) => {
+  
+};*/
+
+
+
+/*router.put('/api/sauces/:id' ,(req,res,next) => {
+  
+});
+
+router.delete('/api/sauces/:id',(req,res,next)) => {
+  
+};
+
+router.post('/api/sauces/:id/like' ,(req,res,next)) => {
+  
+};*/
+  
+
+//1ER MIDDLEWARE
+router.use((req,res,next) => {  //REQ > REQUEST -- RES > RESPONSE
+  res.json({ message : 'Votre requête a été bien reçue!'});
+  next();
+});
+//2EME MIDDLEWARE
+router.use((req,res,next) => {  //REQ > REQUESTE -- RES > RESPONSE
+  res.statut(201);
+});
+
+module.exports = router;
