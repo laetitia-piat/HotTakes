fa3872bc988de3e6e8e8583b56d242a4b9cef77f
laetitia-piat <63997891+laetitia-piat@users.noreply.github.com>
2023-02-21 18:24:41 +0100

    Add files via upload

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.createUser);
-//router.post('/login', userCtrl.connectUser);
-
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
 
 module.exports = router;
